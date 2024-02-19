882fb04b8620adae6e7bc6034afebd74a1e1f722
CaydeXI <bobism2013@gmail.com>
2024-02-19 13:10:31 -0500

    Add home page, ingredients page, input meal page, list page, recipe page classes. Also added home page, ingredients page, input meals page, list page, and recipe page xml files. in the home page class, I added button with all the intents necessary to take user to the corresponding pages. In the xml pages, I added the titles.

---
+++ app/src/main/java/com/example/androidprojecttemplate/ListPage.java
@@ -0,0 +1,16 @@
+package com.example.androidprojecttemplate;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ListPage extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_list_page);
+    }
+}
