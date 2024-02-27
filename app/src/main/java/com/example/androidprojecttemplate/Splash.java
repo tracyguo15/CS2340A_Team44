239f614a10e11eed3e99c4da53323c9be9e383a5
Atominator08 <amv8act@gmail.com>
2024-02-27 17:09:07 -0500

    update the check

--- app/src/main/java/com/example/androidprojecttemplate/Splash.java
+++ app/src/main/java/com/example/androidprojecttemplate/Splash.java
@@ -7,16 +7,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class Splash extends AppCompatActivity {
 
-<<<<<<< Updated upstream:app/src/main/java/com/example/androidprojecttemplate/Splash.java
-    private static int splash = 2000; // 2 seconds of splash
-=======
     private static int splashTime = 2000; // 2 seconds of splash
->>>>>>> Stashed changes:app/src/main/java/com/example/androidprojecttemplate/views/Splash.java
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -24,10 +21,6 @@ public class Splash extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-<<<<<<< Updated upstream:app/src/main/java/com/example/androidprojecttemplate/Splash.java
-        }, splash);
-=======
         }, splashTime);
->>>>>>> Stashed changes:app/src/main/java/com/example/androidprojecttemplate/views/Splash.java
     }
 }
