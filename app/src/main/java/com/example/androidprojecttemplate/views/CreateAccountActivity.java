715db5fa69841c1b137ea3d62b66f0f44e4a77cf
SwampPear <michaeljosephvaden03@gmail.com>
2024-02-27 13:03:30 -0500

    CreateAccountActivity.java is NOW fixed

--- app/src/main/java/com/example/androidprojecttemplate/views/CreateAccountActivity.java
+++ app/src/main/java/com/example/androidprojecttemplate/views/CreateAccountActivity.java
@@ -1,110 +1,89 @@
-package com.example.androidprojecttemplate;
+package com.example.androidprojecttemplate.views;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.view.View;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.androidprojecttemplate.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
 
 public class CreateAccountActivity extends AppCompatActivity {
-    EditText theUsername;
-    EditText thePassword;
-    EditText confirmThePassword;
+    private EditText usernameInput;
+    private EditText passwordInput;
+    private EditText confirmPasswordInput;
+    private Button toHomeScreen;
 
-    FirebaseAuth mAuth;
+    FirebaseAuth firebaseAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        // DO NOT MODIFY
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_account);
-        theUsername = findViewById(R.id.usernameCreate);
-        thePassword = findViewById(R.id.passwordCreate);
-        confirmThePassword = findViewById(R.id.confirmPasswordCreate);
-        Button theButtonToHomeScreen = findViewById(R.id.buttonToHomeScreenFromCreate);
-        // Firebase initialization
-        mAuth = FirebaseAuth.getInstance();
 
-        // Button to create an account
-        theButtonToHomeScreen.setOnClickListener(v -> {
-            String theActualUsername;
-            String theActualPassword;
-            String theActualPasswordConfirmation;
+        usernameInput = findViewById(R.id.usernameInput);
+        passwordInput = findViewById(R.id.passwordInput);
+        confirmPasswordInput = findViewById(R.id.confirmPasswordInput);
+        toHomeScreen = findViewById(R.id.toHomeScreen);
 
-            theActualUsername = String.valueOf(theUsername.getText());
-            theActualPassword = String.valueOf(thePassword.getText());
-            theActualPasswordConfirmation = String.valueOf(confirmThePassword.getText());
+        firebaseAuth = FirebaseAuth.getInstance();
 
-            // Checks whether any of the editTexts are empty or not
-            if (TextUtils.isEmpty(theActualUsername)) {
-                Toast.makeText(createAccountActivity.this, "Please enter a username!", Toast.LENGTH_SHORT).show();
+        toHomeScreen.setOnClickListener(v -> {
+            String username = String.valueOf(usernameInput.getText());
+            String password = String.valueOf(passwordInput.getText());
+            String confirmPassword = String.valueOf(confirmPasswordInput.getText());
+
+            // check validity of username and password
+            if (TextUtils.isEmpty(username)) {
+                Toast.makeText(CreateAccountActivity.this, "Please enter an username!", Toast.LENGTH_SHORT).show();
                 return;
-            } else if (TextUtils.isEmpty(theActualPassword)) {
-                Toast.makeText(createAccountActivity.this, "Please enter a password!", Toast.LENGTH_SHORT).show();
-                return;
-            } else if (TextUtils.isEmpty(theActualPasswordConfirmation)) {
-                Toast.makeText(createAccountActivity.this, "Please enter a password confirmation!", Toast.LENGTH_SHORT).show();
+            } else if (TextUtils.isEmpty(password)) {
+                Toast.makeText(CreateAccountActivity.this, "Please enter a password!", Toast.LENGTH_SHORT).show();
                 return;
             }
 
-            // Checks to make sure that the password matches the confirmation password
-            if (theActualPassword.compareTo(theActualPasswordConfirmation) != 0) {
-                Toast.makeText(createAccountActivity.this, "Password and Password Confirmation don't match!", Toast.LENGTH_SHORT).show();
+            // check that password and password confirmation match
+            if (!TextUtils.equals(password, confirmPassword)) {
+                Toast.makeText(CreateAccountActivity.this, "Passwords do not match!", Toast.LENGTH_SHORT).show();
                 return;
             }
 
-            // check to make sure that the password is more than 6 characters
-            if(theActualPassword.length() < 6) {
-                Toast.makeText(createAccountActivity.this, "Password needs to be 6 or more letters", Toast.LENGTH_SHORT).show();
-                return;
-            }
-
-            // check if email is valid
-            if(!theActualUsername.contains("@")) {
-                Toast.makeText(createAccountActivity.this, "Username has to be a valid email", Toast.LENGTH_SHORT).show();
-                return;
-            }
-
-                // Now we will register the user using firebase
-                mAuth.createUserWithEmailAndPassword(theActualUsername, theActualPassword)
-                        .addOnCompleteListener(createAccountActivity.this, new OnCompleteListener<AuthResult>() {
+            // create user with firebase
+            firebaseAuth.createUserWithEmailAndPassword(username, password)
+            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        
+                        // login with firebase
+                        firebaseAuth.signInWithEmailAndPassword(username, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                 if (task.isSuccessful()) {
-                                    //Toast.makeText(createAccountActivity.this, "Successfull!", Toast.LENGTH_SHORT).show();
-                                    // Switch to new screen
-                                    Intent theIntent = new Intent(createAccountActivity.this, loginPageActivity.class);
-                                    startActivity(theIntent);
+                                    Toast.makeText(CreateAccountActivity.this, "successful", Toast.LENGTH_SHORT).show();
+                                    // switch to home page
+                                    Intent intent = new Intent(CreateAccountActivity.this, HomePage.class);
+                                    startActivity(intent);
                                     finish();
                                 } else {
-                                    Toast.makeText(createAccountActivity.this, "Not Good", Toast.LENGTH_SHORT).show();
-                                    return;
+                                    Toast.makeText(CreateAccountActivity.this, "user login failed", Toast.LENGTH_SHORT).show();
                                 }
                             }
                         });
+                    } else {
+                        Toast.makeText(CreateAccountActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
         });
     }
-
-   /* @Override
-    public void onStart() {
-        super.onStart();
-        FirebaseUser theUser = mAuth.getCurrentUser();
-        if (theUser != null) {
-            Intent theIntent = new Intent(createAccountActivity.this, HomePage.class);
-            startActivity(theIntent);
-        }
-    } */
 }
