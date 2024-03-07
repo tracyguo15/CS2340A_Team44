fc31b63faded63153ff627b6aaf2ad106630eaa4
Dsquared1441 <ddeller3@gatech.edu>
2024-03-07 13:00:49 -0500

    unstably implemented some core UI logic for page, still need functionality to write to/from database

--- app/src/main/java/com/example/androidprojecttemplate/views/PersonalInfo.java
+++ app/src/main/java/com/example/androidprojecttemplate/views/PersonalInfo.java
@@ -8,6 +8,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
@@ -26,6 +30,15 @@ public class PersonalInfo extends AppCompatActivity {
     private static boolean isLoggedIn = false;
 
     private NavigationView nav_view;
+
+    private EditText heightField;
+    private EditText weightField;
+    private RadioGroup genderButtons;
+    private String genderChosen = null;
+
+    private Button saveButton;
+
+    // plan -- add object here later to store demographic data
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -78,6 +91,54 @@ public class PersonalInfo extends AppCompatActivity {
                 return false;
             }
         });
+
+        heightField = findViewById(R.id.heightNumberField);
+        weightField = findViewById(R.id.weightNumberField);
+        genderButtons = findViewById(R.id.genderRadioGroup);
+        saveButton = findViewById(R.id.piiSaveButton);
+
+        //get user data and reflect the values previously defined in the text field
+        //so get user height and prefill it in heightField as an empty field text
+        //similarly get user weight and prefill it in weightField as an empty number
+        //finally, set up gender -- try checking RadioButton.setChecked(boolean checked) for more details
+
+        //now set up listener for gender buttons so that when they are clicked they update genderChosen
+        genderButtons.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton b = findViewById(checkedId);
+
+                genderChosen = getGenderChosen(b);
+            }
+        });
+
+        //and set up save button listener so it:
+        // 1) deactivate the input widgets
+        // 2) save to external database
+        //   2b) gather data from input and
+        //   2a) if it takes too long to connect, reactivate the widget and exit this function
+        // 3) clear all the inputs and prefill with new values
+        // 4) reactivate widgets
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                heightField.setEnabled(false);
+                weightField.setEnabled(false);
+                genderButtons.setEnabled(false);
+
+                //gather data and send it to external database
+                saveToExternalDatabase();
+
+                //prefill with new values, try looking at EditText.setHint(String s) for more details
+                prefillValues();
+
+                //re-enable UI components
+                heightField.setEnabled(true);
+                weightField.setEnabled(true);
+                genderButtons.setEnabled(true);
+            }
+        });
+
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -87,5 +148,38 @@ public class PersonalInfo extends AppCompatActivity {
             nav_view.setVisibility(View.VISIBLE);
         }
         return true || abdt.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A helper method to determine what to set genderChosen to after a radioButton is clicked.
+     *
+     * @param b the RadioButton chosen
+     * @return what gender was selected: "Male", "Female", "Non-Binary", "Other"
+     * */
+    private String getGenderChosen(RadioButton b) {
+        String label = b.getText().toString();
+        if (label.equals("Other/Not Specified")) {
+            return "Other";
+        } else {
+            return label;
+        }
+    }
+
+    /**
+     * This gets the data in the text field and sends it off to the database.
+     * The plan is to update an object in here, probably a ViewModel of some sort,
+     * and then send the results off to the database. If a text field is blank or
+     * no changes have occurred, then exit this function.
+     */
+    private void saveToExternalDatabase() {
+
+    }
+
+    /**
+     * Gets data from the planned Demographic object, and then
+     * prefills it into empty text fields.
+     */
+    private void prefillValues() {
+
     }
 }
