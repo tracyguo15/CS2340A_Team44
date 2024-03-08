fd34f823cd607f5fcd6b8f2cfb390566c866ea3f
Mleonick888 <michaeljleonick@gmail.com>
2024-03-08 00:41:49 -0500

    Got personal data to be shown on the database, associated with different users

--- app/src/main/java/com/example/androidprojecttemplate/views/PersonalInfo.java
+++ app/src/main/java/com/example/androidprojecttemplate/views/PersonalInfo.java
@@ -6,8 +6,12 @@ package com.example.androidprojecttemplate.views;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
@@ -16,7 +20,15 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.example.androidprojecttemplate.R;
+import com.example.androidprojecttemplate.models.personalInfo;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 //implements NavigationView.OnNavigationItemSelectedListener
@@ -27,11 +39,33 @@ public class PersonalInfo extends AppCompatActivity {
 
     private NavigationView nav_view;
 
+    private EditText theHeightInput;
+
+    private EditText theWeightInput;
+
+    private EditText theGenderInput;
+
+    private Button theButtonToLogData;
+
+    // For firebase authentication (to get user's email)
+    FirebaseAuth auth;
+    FirebaseUser user;
+
+
+    // For real-time database
+    FirebaseDatabase rootNode;
+    DatabaseReference reference;
+    DatabaseReference tempReference;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_personal_info_page);
+        theHeightInput = findViewById(R.id.theHeight);
+        theWeightInput = findViewById(R.id.theWeight);
+        theGenderInput = findViewById(R.id.theGener);
+        theButtonToLogData = findViewById(R.id.buttonToEnnterData);
 
         Toolbar homeToolBar = (Toolbar) findViewById(R.id.nav_toolbar);
         setSupportActionBar(homeToolBar);
@@ -78,6 +112,58 @@ public class PersonalInfo extends AppCompatActivity {
                 return false;
             }
         });
+
+        // Get the current user's email, which will be used further down the code
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
+        String theUsersEmail = user.getEmail();
+
+        String[] theNames = new String[15];
+
+        // Will now focus on logging the data
+        theButtonToLogData.setOnClickListener(v -> {
+            // Check if the the inputs from the edit text are valid or not
+            String height = String.valueOf(theHeightInput.getText());
+            String weight = String.valueOf(theWeightInput.getText());
+            String gender = String.valueOf(theGenderInput.getText());
+
+            if (TextUtils.isEmpty(height)) {
+                Toast.makeText(PersonalInfo.this, "Please enter a height!", Toast.LENGTH_SHORT).show();
+                return;
+            } else if (TextUtils.isEmpty(weight)) {
+                Toast.makeText(PersonalInfo.this, "Please enter a weight!", Toast.LENGTH_SHORT).show();
+                return;
+            } else if (TextUtils.isEmpty(gender)) {
+                Toast.makeText(PersonalInfo.this, "Please enter a gender!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            reference = FirebaseDatabase.getInstance().getReference().child("Users");
+            reference.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    for(DataSnapshot theSnapshot: snapshot.getChildren()) {
+
+                        String theEmailFromFirebase = theSnapshot.child("username").getValue().toString();
+                        if (theEmailFromFirebase.equals(theUsersEmail)) {
+                           //Found the email, can now add the data for that specific user
+                            personalInfo theInfo = new personalInfo(height, weight, gender);
+
+                            tempReference = reference.child(theSnapshot.child("name").getValue().toString());
+                            tempReference.child("Personal Info").setValue(theInfo);
+                        }
+
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    Toast.makeText(PersonalInfo.this, "Something went wrong in the outer portion", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+        });
+
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
