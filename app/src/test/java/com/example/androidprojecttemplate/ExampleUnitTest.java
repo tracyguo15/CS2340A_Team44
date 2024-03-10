df88d3746eadcc1a2661d7c055a9a50a113cab1f
tracyguo <tracyyguo15@gmail.com>
2024-03-10 15:54:10 -0400

    Add setters and getters for LoginActivityPage and create tests

--- app/src/test/java/com/example/androidprojecttemplate/ExampleUnitTest.java
+++ app/src/test/java/com/example/androidprojecttemplate/ExampleUnitTest.java
@@ -3,14 +3,17 @@ package com.example.androidprojecttemplate;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+
+import com.example.androidprojecttemplate.views.LoginPageActivity;
+
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
-    @Test
-    public void unitTest() {
+//    @Test
+//    public void unitTest() {
         // initalizes the model with 0 for both sleep and fittness time
 //        WellnessViewModel test = new WellnessViewModel();
 //        assertEquals(0, test.getWellnessData().getSleepHours());
@@ -24,6 +27,22 @@ public class ExampleUnitTest {
 //        assertEquals(88, test.getWellnessData().getSleepHours());
 //        assertEquals(12, test.getWellnessData().getFitnessMinutes());
 //        System.out.println("JUNITS have been completed.");
-    }
+//    }
+
+    //Tracy
+//    @Test
+//    public void nullUsername() {
+//        LoginPageActivity login = new LoginPageActivity();
+//        login.setUsername(null);
+//        assertEquals(null, login.getUsername());
+//    }
+//
+//    //Tracy
+//    @Test
+//    public void nullPassword() {
+//        LoginPageActivity login = new LoginPageActivity();
+//        login.setPassword(null);
+//        assertEquals(null, login.getPassword());
+//    }
 
 }
