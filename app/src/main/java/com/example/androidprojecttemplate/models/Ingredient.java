15e4f2d6b56b76d4791d6e18ef54bb34a91b26d7
Dsquared1441 <ddeller3@gatech.edu>
2024-03-20 09:07:56 -0400

    implemented Ingredient class

---
+++ app/src/main/java/com/example/androidprojecttemplate/models/Ingredient.java
@@ -0,0 +1,37 @@
+package com.example.androidprojecttemplate.models;
+
+public class Ingredient {
+    private double price;
+    private int calories;
+    private int quantity;
+
+    private String name;
+
+    public Ingredient(String name, double price, int calories, int quantity) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("Name is null or blank, please enter a proper value");
+        }
+        if (price < 0) {
+            throw new IllegalArgumentException("The price is negative, please enter a nonnegative number.");
+        }
+        if (calories < 0) {
+            throw new IllegalArgumentException("Ingredient must have nonnegative number of calories.")
+        }
+        if (quantity < 0) {
+            throw new IllegalArgumentException("The quantity must be nonnegative integer.");
+        }
+        this.name = name;
+        this.price = price;
+        this.calories = calories;
+        this.quantity = quantity;
+    }
+    public void consume(int quantity) { }
+
+    public void replenish(int quantity) { }
+
+    public int getQuantity() {}
+
+    public double getPrice() {}
+
+    public String getName() {}
+}
