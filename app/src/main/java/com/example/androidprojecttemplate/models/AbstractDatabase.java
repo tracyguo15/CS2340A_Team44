54cc8f37ea5b4291278548a38eaf49c1fb569f4d
Dsquared1441 <ddeller3@gatech.edu>
2024-03-20 09:42:16 -0400

    implemented AbstractDatabase, but removed its Singleton status

---
+++ app/src/main/java/com/example/androidprojecttemplate/models/AbstractDatabase.java
@@ -0,0 +1,27 @@
+package com.example.androidprojecttemplate.models;
+
+import java.util.Hashtable;
+
+public class AbstractDatabase<K,V> {
+    private Hashtable<K,V> table;
+
+    public AbstractDatabase() {
+        table = new Hashtable<>();
+    }
+
+    public V get(K key) {
+        return table.get(key);
+    }
+
+    public V put(K key, V val) {
+        return table.put(key, val);
+    }
+
+    public V remove(K key) {
+        return table.remove(key);
+    }
+
+    public int size() {
+        return table.size();
+    }
+}
