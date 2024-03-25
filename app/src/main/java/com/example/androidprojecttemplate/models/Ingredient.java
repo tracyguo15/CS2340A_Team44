3215ffa533a02c860470d7ff924ebb83ba30684b
SwampPear <michaeljosephvaden03@gmail.com>
2024-03-24 23:25:42 -0400

    removed quantity from Ingredient model-redundant

--- app/src/main/java/com/example/androidprojecttemplate/models/Ingredient.java
+++ app/src/main/java/com/example/androidprojecttemplate/models/Ingredient.java
@@ -3,35 +3,27 @@ package com.example.androidprojecttemplate.models;
 public class Ingredient {
     private double price;
     private int calories;
-    private int quantity;
 
     private String name;
 
-    public Ingredient(String name, double price, int calories, int quantity) {
+    public Ingredient(String name, double price, int calories) {
         if (name == null) {
             throw new IllegalArgumentException("Name is null, please enter a proper value");
         }
         if (price < 0) {
-            throw new IllegalArgumentException("The price is negative, please enter a nonnegative number.");
+            throw new IllegalArgumentException("The price is negative, please enter a positive number.");
         }
         if (calories < 0) {
-            throw new IllegalArgumentException("Ingredient must have nonnegative number of calories.");
+            throw new IllegalArgumentException("Ingredient must have positive number of calories.");
         }
-        if (quantity < 0) {
-            throw new IllegalArgumentException("The quantity must be nonnegative integer.");
-        }
+
         this.name = name;
         this.price = price;
         this.calories = calories;
-        this.quantity = quantity;
     }
     public void consume(int quantity) { }
 
     public void replenish(int quantity) { }
-
-    public int getQuantity() {
-        return quantity;
-    }
 
     public double getPrice() {
         return price;
