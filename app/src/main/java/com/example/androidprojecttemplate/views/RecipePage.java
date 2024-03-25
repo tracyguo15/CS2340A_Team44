280071a7611e731db70379347f889116abb376ac
SwampPear <michaeljosephvaden03@gmail.com>
2024-03-24 22:54:23 -0400

    setup RecipePage from InputMealPage

--- app/src/main/java/com/example/androidprojecttemplate/views/RecipePage.java
+++ app/src/main/java/com/example/androidprojecttemplate/views/RecipePage.java
@@ -1,5 +1,6 @@
 package com.example.androidprojecttemplate.views;
 
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -15,22 +16,74 @@ import com.example.androidprojecttemplate.R;
 import com.google.android.material.navigation.NavigationView;
 
 public class RecipePage extends AppCompatActivity {
-    // navbar
-    //private DrawerLayout dl;
-    //private ActionBarDrawerToggle abdt;
-    //private NavigationView navView;
-    private Nav nav;
+    // ui
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle abdt;
+
+    private NavigationView navView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_recipe_page);
+
+        // navbar
+        Toolbar homeToolBar = (Toolbar) findViewById(R.id.nav_toolbar);
+        setSupportActionBar(homeToolBar);
+
+        // nav menu
+        dl = (DrawerLayout) findViewById(R.id.dl);
+        abdt = new ActionBarDrawerToggle(this, dl, R.string.Open, R.string.Close);
+        abdt.setDrawerIndicatorEnabled(true);
+
+        dl.addDrawerListener(abdt);
+        abdt.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        navView = (NavigationView) findViewById(R.id.nav_view);
+        navView.setVisibility(View.GONE);
+        navView.setNavigationItemSelectedListener(new NavigationView
+                .OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                System.out.println(R.id.inputmeal);
+                int id = item.getItemId();
+                if (id == R.id.inputmeal) {
+                    if (!(RecipePage.this instanceof RecipePage)) {
+                        Intent intent = new Intent(RecipePage.this, InputMealPage.class);
+                        startActivity(intent);
+                    }
+                    return true;
+                } else if (id == R.id.recipe) {
+                    Intent intent = new Intent(RecipePage.this, RecipePage.class);
+                    startActivity(intent);
+                    return true;
+                } else if (id == R.id.ingredient) {
+                    Intent intent = new Intent(RecipePage.this, IngredientPage.class);
+                    startActivity(intent);
+                    return true;
+                } else if (id == R.id.list) {
+                    Intent intent = new Intent(RecipePage.this, ListPage.class);
+                    startActivity(intent);
+                    return true;
+                } else if (id == R.id.personalinfo) {
+                    Intent intent = new Intent(RecipePage.this, PersonalInfo.class);
+                    startActivity(intent);
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-
+        if (navView.getVisibility() == View.VISIBLE) {
+            navView.setVisibility(View.GONE);
+        } else {
+            navView.setVisibility(View.VISIBLE);
+        }
         return true;
     }
 }
