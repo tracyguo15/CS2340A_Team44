f8c84c4ff2201631b1aa1bddf2e1adc445404f7a
Rohan B. <rbhole237@gmail.com>
2024-03-31 22:27:33 -0400

    Added the name variable back into RecipeData, don't really know why it was removed. Added test cases for Adam, Michael Vaden, and I. Also ran checkstyle on Michael Vaden's code.

--- app/src/main/java/com/example/androidprojecttemplate/models/RecipeData.java
+++ app/src/main/java/com/example/androidprojecttemplate/models/RecipeData.java
@@ -1,14 +1,20 @@
 package com.example.androidprojecttemplate.models;
 
-import java.util.ArrayList;
+//import java.util.ArrayList;
 
 public class RecipeData extends AbstractDatabase<String, Integer> {
+    private String name;
     private String description;
     private int time;
 
-    public RecipeData(String description, int time) {
+    public RecipeData(String name, String description, int time) {
+        this.name = name;
         this.description = description;
         this.time = time;
+    }
+
+    public RecipeData(String name, int time) {
+        this(name, "", time);
     }
 
     public void add(String ingredientName, int quantity) {
@@ -19,9 +25,34 @@ public class RecipeData extends AbstractDatabase<String, Integer> {
         this.remove(ingredientName);
     }
 
-    public void setTime(int time) { this.time = time; }
-    public void setDescription(String description) { this.description = description; }
+    public void setName(String name) {
+        try {
+            this.name = name;
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("Invalid name. "
+                    + "Please try another one.");
+        }
+    }
+    public void setTime(int time) {
+        try {
+            this.time = time;
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("Invalid time. "
+             + "Please try a positive value.");
+        }
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-    public int getTime() { return this.time; }
-    public String getDescription() { return this.description; }
+    //Getters
+    public String getName() {
+        return this.name;
+    }
+    public int getTime() {
+        return this.time;
+    }
+    public String getDescription() {
+        return this.description;
+    }
 }
