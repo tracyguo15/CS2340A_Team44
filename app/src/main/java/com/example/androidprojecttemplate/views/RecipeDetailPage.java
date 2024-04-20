507828c048cb17ed22dab5e389a4acc7ca3dc8ff
Rohan B. <rbhole237@gmail.com>
2024-04-20 17:08:22 -0400

    Fixed up the RecipeDetailPage to display correct information and added a cook button but the functionality is not yet implemented

--- app/src/main/java/com/example/androidprojecttemplate/views/RecipeDetailPage.java
+++ app/src/main/java/com/example/androidprojecttemplate/views/RecipeDetailPage.java
@@ -2,15 +2,20 @@ package com.example.androidprojecttemplate.views;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.androidprojecttemplate.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class RecipeDetailPage extends AppCompatActivity {
     //Code to display the data
@@ -40,8 +45,6 @@ public class RecipeDetailPage extends AppCompatActivity {
             startActivity(intent);
         });
 
-        /*
-
         //Define the button to cook the recipe
         cookBtn = findViewById(R.id.cookBtn);
         cookBtn.setOnClickListener(v -> {
@@ -50,23 +53,47 @@ public class RecipeDetailPage extends AppCompatActivity {
 
         //Retrieve the name of the recipe that was selected
         Intent intent = getIntent();
-        String recipeKey = (String) intent.getSerializableExtra("recipe");
+        String recipeKey = (String) intent.getSerializableExtra("recipeName");
+
+        Log.d("RECIPE KEY", recipeKey);
+
 
         //Creates a reference to the recipe in the Firebase Database
         recipeDatabase = FirebaseDatabase.getInstance().getReference().child("Cookbook")
                 .child(recipeKey);
 
+        recipeDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                //Displays the recipe's name
+                String recipeNameText = snapshot.getKey();
+                recipeName.setText(recipeNameText);
+
+                //Displays the recipe's description
+                String recipeDescText = "Description: " + snapshot.child("description").getValue();
+                recipeDescription.setText(recipeDescText);
+
+                //Displays the recipe's cook time
+                String recipeTimeText = "Total time: " + snapshot.child("time").getValue();
+                recipeTime.setText(recipeTimeText);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.d("RECIPE ERROR", error.toString());
+            }
+        });
+        /*
         //Displays the recipe's name
-        String recipeNameText = "Name: " + recipeDatabase.getKey();
+        String recipeNameText = recipeDatabase.getKey();
         recipeName.setText(recipeNameText);
 
         //Displays the recipe's description
-        String recipeDescText = "Description: " + recipeDatabase.child("description")
-                .get().toString();
+        String recipeDescText = "Description: " + recipeDatabase.chi;
         recipeDescription.setText(recipeDescText);
 
         //Displays the recipe's cook time
         String recipeTimeText = "Total time: " + recipeDatabase.child("time").get().toString();
-        recipeTime.setText(recipeTimeText);*/
+        recipeTime.setText(recipeTimeText); */
     }
 }
